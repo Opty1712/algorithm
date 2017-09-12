c25341473be5dcde82e10496384ae2a62d8044f6
Едунов Андрей Борисович <SBT-Edunov-AB@sigma.sbrf.ru>
2017-09-12 17:12:00 +0300

    final

---
+++ script.js
@@ -0,0 +1,19 @@
+const arr = [70, 10, 20, 30, 40, 50, 60, 30, 40, 50, 60, 0];
+
+const summ = 100;
+
+const findDigits = (arr) => {
+  const ret = [];
+  arr.reduce((prev, curr) => {
+    if (prev[curr]) return prev;
+    
+    prev[curr] = true;
+    const dif = summ - curr;
+    if (prev[dif]) ret.push([curr, dif]);
+    
+    return prev;
+  }, {});
+  return ret;
+}
+
+console.log(findDigits(arr));
